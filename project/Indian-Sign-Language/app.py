a23fde0bd231ce0502590a0888a2d4eccd7f2f3b
Dimpal <dimpalalokar@gmail.com>
2025-10-25 13:45:40 +0530

    Ready for deployment

--- project/Indian-Sign-Language/app.py
+++ project/Indian-Sign-Language/app.py
@@ -159,5 +159,5 @@ def get_sentence():
 
 
 # ---------------- Run App ----------------
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
